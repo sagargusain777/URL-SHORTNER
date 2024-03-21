a79b582f5b1ec312fec4763542bdee369c9d7229
uwuxverticuse <sagargusain150@gmail.com>
2024-03-21 23:38:40 +0530

    ✅ Model for Url

---
+++ src/models/url.models.js
@@ -0,0 +1,16 @@
+import mongoose from mongoose;
+
+const urlSchema = new mongoose.Schema({
+    shortId :{
+        type : String,
+        required : true,
+        unique : true,
+    },
+    redirectUrl :{
+        type : String,
+        required : true,
+    },
+    vistHistory : [{timestamps: {type : Number}}],
+},{timestamps: true});
+
+export const URL = mongoose.model('URL',urlSchema);
