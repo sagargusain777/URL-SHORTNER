e600fcce7cbe00c9158f191b40a9faf273e07aa7
uwuxverticuse <sagargusain150@gmail.com>
2024-03-22 00:29:00 +0530

    ✅ Logic for the Url generation

---
+++ src/controllers/url.controllers.js
@@ -0,0 +1,19 @@
+import {nanoid} from 'nanoid';
+
+ import URL from '../models/url.models';
+async function handleGenerateNewShortUrl(req,res){
+    const body = req.body;
+    if(!body.url){
+        return  res.status(400).json({error: 'URL is required'})
+    }
+    const shortID = nanoid(9);
+    await URL.create({
+        shortId : shortID,
+        redirectUrl : body.url,
+        visitHistory : []
+    })
+
+    return res.json({id : shortID})
+
+}
+export default handleGenerateNewShortUrl;
