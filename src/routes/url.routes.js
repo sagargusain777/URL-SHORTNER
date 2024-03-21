f7b17ea21e6a65ef141aecf0fb52e2c74f98f0c5
uwuxverticuse <sagargusain150@gmail.com>
2024-03-22 00:28:29 +0530

    📝 Home Route for Url

---
+++ src/routes/url.routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {handleGenerateNewShortUrl} from '../controllers/url.controllers.js'
+const router = express.Router();
+
+
+router.post('/api',handleGenerateNewShortUrl)
+
+export default router;
