ac4145189f6b7ef46c988ffe0e22b8439c950d58
uwuxverticuse <sagargusain150@gmail.com>
2024-03-23 01:48:57 +0530

    ✅ Add  the shortId  route

--- src/index.js
+++ src/index.js
@@ -5,11 +5,27 @@ dotenv.config({
 import express from "express";
 import urlRoute from "./routes/url.routes.js";
 import connectDB from "./db/db.js";
-
+import {URL} from './models/url.models.js'
 const app = express();
 const PORT =process.env.PORT
 app.use(express.json({limit :"16kb"}))
 app.use("/url", urlRoute);
+
+app.get('/:shortId',async(req,res)=>{
+       const shortId = req.params.shortId;
+
+     const entry =  await URL.findOneAndUpdate({
+        shortId
+
+       },{$push : {
+        vistHistory : {
+          timestamps : Date.now()
+        }
+       }})
+
+       res.redirect(entry.redirectUrl);
+
+})
 
 
 connectDB()
