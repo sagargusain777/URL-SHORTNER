b28f2fb00564a69c72e81b08b56db0193d1505d5
uwuxverticuse <sagargusain150@gmail.com>
2024-03-24 01:49:46 +0530

    ⚡️ Imporve code

--- src/controllers/url.controllers.js
+++ src/controllers/url.controllers.js
@@ -1,29 +1,46 @@
-import {nanoid} from 'nanoid';
+import { nanoid } from "nanoid";
 
- import {URL} from '../models/url.models.js';
-export const handleGenerateNewShortUrl= async(req,res)=>{
+import { URL } from "../models/url.models.js";
+
+// New URL creation in the database and giving the short id to the original URL
+export const handleGenerateNewShortUrl = async (req, res) => {
+  
+  try {
     const body = req.body;
-    if(!body.url){
-        return  res.status(400).json({error: 'URL is required'})
-    }
+  if (!body.url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
     const shortID = nanoid(9);
-    await URL.create({
-        shortId : shortID,
-        redirectUrl : body.url,
-        visitHistory : []
-    })
+  await URL.create({
+    shortId: shortID,
+    redirectUrl: body.url,
+    visitHistory: [],
+  });
 
-    return res.json({id : shortID})
+  return res.json({ id: shortID });
+    
+  } catch (error) {
+    console.log(`Error generating short URL : ${error.message}`);
+    return res.status(500).json({ error: "Internal Server Error" });
+    
+  }
+  
+};
+//Function for analytics data of the short URL
+export const handleAnalyticsData = async (req, res) => {
+  const shortId = req.params.shortId;
+  console.log(shortId);
 
-}
+  try {
+    const analyticsData = await URL.findOne({ shortId });
 
-export  const handleAnalyticsData = async(req,res)=>{
-
-    const shortId = req.params.shortId;
-    console.log(shortId)
-
-    const analyticsData = await URL.findOne({shortId});
-
-    res.status(200).send(` total Clicks :${analyticsData.visitHistory.length}  Analytics : ${analyticsData.visitHistory} `)
-
-}
+    res
+      .status(200)
+      .send(
+        ` total Clicks :${analyticsData.visitHistory.length} Analytics : ${analyticsData.visitHistory} `
+      );
+  } catch (error) {
+    console.log(`Error fetching analytics data : ${error.message}`);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
